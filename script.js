e8d991d28ead64d85a72a11827dc2dc79b781e34
Adam And <andadam24@gmail.com>
2021-09-14 18:30:47 -0400

    updated global variables

---
+++ script.js
@@ -0,0 +1,8 @@
+var search = document.getElementById("input-group");
+var city = document.getElementById("weatherToday");
+var forecast = document.getElementsByClassName("forecast");
+var apiKey = 'bab1286e896abbf2b3fbf496f8054549';
+var searchArr = [];
+
+
+
