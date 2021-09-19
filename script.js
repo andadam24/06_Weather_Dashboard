70904402ee9afc65f30a283bd86f54e1291bef40
Adam And <andadam24@gmail.com>
2021-09-19 17:21:30 -0400

    hopefully got form handler code

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ var cityBtn = document.querySelector('#search-btn');
 var cityNameEl = document.querySelector('#city-name');
 var cityArr = [];
 var apiKey = 'bab1286e896abbf2b3fbf496f8054549'
+
+var Handler = function(event) {
+    var selectedCity = cityInput
+        .value
+        .trim()
+        .toLowerCase()
+        .split(' ')
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ');
+
+    if (selectedCity) {
+        getCoords(selectedCity);
+        cityInput.value = '';
+    } else {
+        alert('Please enter a city!');
+    };
+};
