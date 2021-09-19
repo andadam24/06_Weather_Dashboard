c0f14a7a136e3dda35cfefd34a05d293c903f197
Adam And <andadam24@gmail.com>
2021-09-19 17:01:40 -0400

    setting up variables for back-end

---
+++ script.js
@@ -0,0 +1,5 @@
+var cityInput = document.querySelector('#city-input');
+var cityBtn = document.querySelector('#search-btn');
+var cityNameEl = document.querySelector('#city-name');
+var cityArr = [];
+var apiKey = 'bab1286e896abbf2b3fbf496f8054549'
